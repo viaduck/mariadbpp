7a87976e09ef14ddf171cd7be0242ad75d0ff0d4
Yannick Pflanzer <ypflanzer@outlook.com>
2016-07-04 19:07:13 +0200

    Implemented AlterTable tests

---
+++ testsrc/AlterTableTest.cpp
@@ -0,0 +1,42 @@
+#include "AlterTableTest.h"
+
+void AlterTableTest::CreateTestTable() {
+    m_con->execute("CREATE TABLE " + m_table_name +
+                   " (id INT AUTO_INCREMENT, PRIMARY KEY (`id`));");
+}
+
+TEST_F(AlterTableTest, AlterAddAndDrop_existing) {
+    m_con->execute("ALTER TABLE " + m_table_name + " ADD new INT;");
+    m_con->execute("ALTER TABLE " + m_table_name + " DROP COLUMN new;");
+}
+
+TEST_F(AlterTableTest, AlterAddAndModify_existing) {
+    m_con->execute("ALTER TABLE " + m_table_name + " ADD new INT;");
+    m_con->execute("ALTER TABLE " + m_table_name + " MODIFY new DOUBLE NOT NULL;");
+}
+
+TEST_F(AlterTableTest, AlterAdd_non_existing_table) {
+    EXPECT_ANY_THROW(m_con->execute("ALTER TABLE doesntexist ADD new INT;"));
+}
+
+TEST_F(AlterTableTest, AlterDrop_non_existing_table) {
+    EXPECT_ANY_THROW(m_con->execute("ALTER TABLE doesntexist DROP COLUMN new;"));
+}
+
+TEST_F(AlterTableTest, AlterModify_non_existing_table) {
+    EXPECT_ANY_THROW(m_con->execute("ALTER TABLE doesntexist MODIFY new DOUBLE;"));
+}
+
+TEST_F(AlterTableTest, AlterModify_non_existing_column) {
+    m_con->execute("ALTER TABLE " + m_table_name + " ADD new INT;");
+    EXPECT_ANY_THROW(
+        m_con->execute("ALTER TABLE " + m_table_name + " MODIFY new_doesntexist DOUBLE;"));
+}
+
+TEST_F(AlterTableTest, AlterAddAndDrop_no_connect) {
+    account_ref no_acc = account::create("256.256.256.256", "", "");
+    connection_ref no_conn = connection::create(no_acc);
+
+    EXPECT_ANY_THROW(no_conn->execute("ALTER TABLE " + m_table_name + " ADD new INT;"));
+    EXPECT_ANY_THROW(no_conn->execute("ALTER TABLE " + m_table_name + " DROP COLUMN new;"));
+}
---
+++ testsrc/AlterTableTest.h
@@ -0,0 +1,11 @@
+#ifndef __ALTER_TABLE_TEST_H
+#define __ALTER_TABLE_TEST_H
+
+#include "SkeletonTest.h"
+
+class AlterTableTest : public SkeletonTest {
+   public:
+    virtual void CreateTestTable() override;
+};
+
+#endif
