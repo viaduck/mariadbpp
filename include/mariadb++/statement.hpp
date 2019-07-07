4b698f723a74c0d40a6d18c97ead2cfe11f23684
Alexandros Roussos <thealexroussos@gmail.com>
2019-07-07 02:57:26 +0200

    Removed statement::set_enum
    Since set_enum cannot be implemented properly
    we decided to remove it. See gh-24

--- include/mariadb++/statement.hpp
+++ include/mariadb++/statement.hpp
@@ -76,7 +76,6 @@ class statement : public last_error {
     MAKE_SETTER_DECL(data, const data_ref&);
     MAKE_SETTER_DECL(decimal, const decimal&);
     MAKE_SETTER_DECL(string, const std::string&);
-    MAKE_SETTER_DECL(enum, const char*);
     MAKE_SETTER_DECL(boolean, bool);
     MAKE_SETTER_DECL(unsigned8, u8);
     MAKE_SETTER_DECL(signed8, s8);
